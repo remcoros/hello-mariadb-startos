810ab4ea2e9330eeea438bdda6bd517883855c37
Remco Ros <remcoros@live.nl>
2024-07-15 17:25:03 +0000

    fix: correct logic to read/set mysql root password env variable and some minor cleanup

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,47 +2,40 @@
 
 set -ea
 
-_term() {
-    echo "Caught TERM signal!"
-    kill -TERM "$app_process" 2>/dev/null
-    kill -TERM "$dbgate_process" 2>/dev/null
-    kill -TERM "$db_process" 2>/dev/null    
-}
-
 echo
 echo "Starting Hello MariaDB..."
 echo
 
 # Setup MariaDB
-if [ -d "/run/mysqld" ]; then
-    echo "[i] mysqld already present, skipping creation"
-    chown -R mysql:mysql /run/mysqld
-else
-    echo "[i] mysqld not found, creating...."
-    mkdir -p /run/mysqld
-    chown -R mysql:mysql /run/mysqld
-fi
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
+# check if the 'mysql' system database exists
 if [ -d /var/lib/mysql/mysql ]; then
     echo "[i] MariaDB directory already present, skipping creation"
     chown -R mysql:mysql /var/lib/mysql
+
+    # get root password from stats file, we'll need it later. The stats file is created and updated on every run (and included in backups)
+    MYSQL_ROOT_PASSWORD=$(yq e '.data.["MariaDB root password"].value' /root/data/start9/stats.yaml)
 else
     echo "[i] MariaDB data directory not found, creating initial DBs"
 
     mkdir -p /var/lib/mysql
     chown -R mysql:mysql /var/lib/mysql
 
+    # install system db
     mysql_install_db --user=mysql --ldata=/var/lib/mysql >/dev/null
 
+    # generate the root password
     if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-        MYSQL_ROOT_PASSWORD=`pwgen 16 1`
+        export MYSQL_ROOT_PASSWORD=`pwgen 16 1`
         echo "[i] MariaDB root Password: $MYSQL_ROOT_PASSWORD"
-        export MYSQL_ROOT_PASSWORD
     fi
 
-    MYSQL_DATABASE=${MYSQL_DATABASE:-"dbgate"}
-	MYSQL_USER=${MYSQL_USER:-"dbgate"}
-	MYSQL_PASSWORD=${MYSQL_PASSWORD:-"dbgate"}
+    # create a database and give privileges for the 'app'
+    MYSQL_DATABASE=${MYSQL_DATABASE:-"app"}
+	MYSQL_USER=${MYSQL_USER:-"app"}
+	MYSQL_PASSWORD=${MYSQL_PASSWORD:-"app"}
 
     tfile=$(mktemp)
     if [ ! -f "$tfile" ]; then
@@ -72,6 +65,7 @@ EOF
 	    fi
 	fi
 
+    # run the script
     /usr/sbin/mysqld --user=mysql --datadir='/var/lib/mysql' --bootstrap --verbose=0 --skip-networking=0 <$tfile
     rm -f $tfile
 
@@ -80,9 +74,7 @@ EOF
     echo
 fi
 
-# Update properties
-
-MYSQL_ROOT_PASSWORD=$(yq e '.data.["MariaDB root password"].value' /root/data/start9/stats.yaml)
+# Update stats (properties) file
 
 mkdir -p /root/data/start9
 cat <<EOF >/root/data/start9/stats.yaml
@@ -133,10 +125,19 @@ cd /home/dbgate-docker
 node bundle.js --listen-api &
 dbgate_process=$!
 
-# run our demo App
+# run our demo 'App'
 
 lighttpd -f /etc/lighttpd/lighttpd.conf &
 app_process=$!
+
+# hook the TERM signal and wait for all our processes
+
+_term() {
+    echo "Caught TERM signal!"
+    kill -TERM "$app_process" 2>/dev/null
+    kill -TERM "$dbgate_process" 2>/dev/null
+    kill -TERM "$db_process" 2>/dev/null    
+}
 
 trap _term TERM
 wait $db_process $dbgate_process $app_process
